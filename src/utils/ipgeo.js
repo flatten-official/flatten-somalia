fb4378092d29246cfd13c5c7935dfe881e3b6142
Arthur Allshire <arthur@allshire.org>
2020-04-10 11:06:23 -0400

    Fix handling of failed request

--- src/utils/ipgeo.js
+++ src/utils/ipgeo.js
@@ -7,9 +7,14 @@ var ip_geo_secret = new secrets.Secret(process.env.IPGEO_SECRET);
 
 ipgeo = async (request_ip) => {
   let api_key = await ip_geo_secret.get();
-  let res = await axios.get(`${ip_geo_url}?apiKey=${api_key}&ip=${request_ip}` ).catch(console.error);
-
   let locale = "en";
+
+  try {
+    let res = await axios.get(`${ip_geo_url}?apiKey=${api_key}&ip=${request_ip}`);
+  } catch(e) {
+    console.log("Failed to get IP geo info");
+  }
+
   if ('country_name' in res.data) {
     if (res.data.country_name === "United States") {
       locale = "enUS";
