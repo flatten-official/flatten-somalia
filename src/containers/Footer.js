b7c18fb7f555b835eea60d9ef989f0a8bd799ed4
Martin Staadecker <machstg@gmail.com>
2020-05-12 01:33:58 -0400

    Minor refactor

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,15 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer">
+export default () => (
+    <footer className="footer">
         <div className="container text-center" style={{padding: '15px'}}>
-          Built by&nbsp;
-          <a href="https://flatten.ca">Flatten.ca</a>&nbsp;
-          |&nbsp;Forms powered by <a href="https://form.io">form.io</a>
+            Built by&nbsp;
+            <a href="https://flatten.ca">Flatten.ca</a>&nbsp;
+            |&nbsp;Forms powered by <a href="https://form.io">form.io</a>
         </div>
-      </footer>
-    );
-  }
-}
+    </footer>
+);
--- src/views/Admin/Admin.js
+++ src/views/Admin/Admin.js
@@ -1,42 +1,33 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import {PropTypes} from 'prop-types';
-import { selectRoot } from "react-formio";
+import {selectRoot} from "react-formio";
 import VolunteerForm from "../Form/VolunteerForm"
 import {FormConfig} from '../../config'
 
 const Admin = class extends Component {
-  static propTypes = {
-    auth: PropTypes.object.isRequired
-  };
+    static propTypes = {
+        auth: PropTypes.object.isRequired
+    };
 
 
-  render() {
-    const {auth} = this.props;
-    let isAdmin = (auth.is.hasOwnProperty('administrator') && auth.is.administrator)
-    return (
-        <div>
-            {isAdmin ? 
-            <div> Welcome to the admin panel!
-                <VolunteerForm {...FormConfig.addVolunteerForm} />
+    render() {
+        const {auth} = this.props;
+        let isAdmin = (auth.is.hasOwnProperty('administrator') && auth.is.administrator)
+        return (
+            <div>
+                {isAdmin ?
+                    <div> Welcome to the admin panel!
+                        <VolunteerForm {...FormConfig.addVolunteerForm} />
+                    </div>
+                    : <div> Not authorised </div>}
             </div>
-            : <div> Not authorised </div>}
-        </div>
-    );
-  }
+        );
+    }
 }
 
-const mapStateToProps = (state) => {
-  return {
+const mapStateToProps = (state) => ({
     auth: selectRoot('auth', state)
-  }
-}
+});
 
-const mapDispatchToProps = () => {
-  return {};
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Admin)
+export default connect(mapStateToProps)(Admin)
