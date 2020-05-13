d46c2ea3f9a288c16425467b1c696b812a314d54
Arthur Allshire <arthur@allshire.org>
2020-05-12 23:17:48 -0400

    more fixes volunteer middleware

--- volunteerMiddleware.js
+++ volunteerMiddleware.js
@@ -8,19 +8,19 @@ USER_RESOURCE_NAME=process.env.USER_RESOURCE_NAME;
 
 exports.authenticateLogin = (req, res) => {
   if (!(req.headers.token === REQUEST_TOKEN)) {
-      res.status(401).send("401 Forbidden.");
+      res.status(401).send("404 Forbidden.");
   }
-  console.log(req.query.email);
+  let url = `${API_URL}/${USER_RESOURCE_NAME}/exists?data.email=${req.query.email}`
   axios({
-    url: `${API_URL}/${USER_RESOURCE_NAME}/exists?data.email=${req.query.email}`,
+    url: url,
     method: 'get',
-    headers: { 'x-token': TOKEN, 'Content-Type': 'application/json'},
+    headers: { 'x-token': FORMIO_TOKEN, 'Content-Type': 'application/json'},
     data: {
     }
   }).then((response)=>{
         res.sendStatus(200);
     }
-  ).catch(()=>{
+  ).catch((e)=>{
       // TODO - change this so that the failure is silent - this is a security hole.
       res.status(401).send('No email sent: please contact an admin to obtain access.');
     });
