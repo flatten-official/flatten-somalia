ef7aeabfcf448967701943ef8df355b8e278d7ca
Arthur Allshire <arthur@allshire.org>
2020-05-20 11:01:46 -0400

    working on securing express

---
+++ src/app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const app = express();
+
+const routes = require("./routes");
+const utils = require("./utils/secrets");
+
+var cookieSecret = new utils.Secret(process.env.COOKIE_SECRET);
+
+exports.appPromise = cookieSecret.load().then(() => {
+  if (process.env.environment === "dev") {
+    app.use(cors({ origin: true, credentials: true }));
+  } else {
+    app.use(
+      cors({
+        origin: [`https://${process.env.DOMAIN}`],
+        credentials: true,
+      })
+    );
+  }
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.raw());
+  app.use(express.json());
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.use(cookieParser(cookieSecret.secret));
+  app.use("/", routes);
+
+  console.log(cookieSecret.secret);
+  return app;
+});
--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const routes = require("./routes");
 const express = require("express");
 
 const app = express();
+var appPromise = require("./app").appPromise;
 
 app.use("/", routes);
 
-app.listen(port, () => {
-  console.log(`listening on port ${port}.`);
+appPromise.then(function (app) {
+  app.listen(port, () => {
+    console.log(`listening on port ${port}.`);
+  });
 });
