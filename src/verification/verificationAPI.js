5ca4de22ca5c5ee901b56e673a784864affb5fcb
Martin Staadecker <machstg@gmail.com>
2020-05-20 18:18:45 -0400

    Add verificationAPI stub

--- src/verification/verificationAPI.js
+++ src/verification/verificationAPI.js
@@ -1,3 +1,25 @@
-function verifyTokenAndMakeCookie() {
+const { calculateExpiryTime } = require("./../utils/time");
+const { writeCookie } = require("./cookieData");
+
+const COOKIE_LIFE = 1080; // In minutes
+
+/**
+ *
+ * @param tokenValue
+ * @return a cookieID to pass to the user or null if
+ */
+async function verifyTokenAndMakeCookie(tokenValue) {
   // TODO Verify token
+  const isValidToken = true;
+
+  if (!isValidToken) return null;
+
+  // TODO Read volunteerID from token
+  const volunteerID = null;
+
+  const cookieExpiry = calculateExpiryTime(COOKIE_LIFE);
+
+  return await writeCookie(cookieExpiry, volunteerID);
 }
+
+module.exports = { verifyTokenAndMakeCookie };
