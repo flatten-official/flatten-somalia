a8e2a76d943bdcb5738944355eb7de4ba4a55f88
Arthur Allshire <arthur@allshire.org>
2020-05-24 16:30:51 -0400

    update header to use react bootstrap

--- src/translations/en/Navbar.js
+++ src/translations/en/Navbar.js
@@ -2,6 +2,9 @@ exports.default = {
   links: {
     adminPanel: "Admin Panel",
     loggedInAndLogout: "Logged in as %{user}. | Logout",
-    login: "Login",
+    login: "Log in",
+    logout: "Log out",
+    home: "Home",
+    submitForm: "Submit Form",
   },
 };
