cfc9d49f8d0c0386593ed2bb12232d2a234f6411
Arthur Allshire <arthur@allshire.org>
2020-05-24 14:46:11 -0400

    Add placeholder translations

--- src/translations/en/Home.js
+++ src/translations/en/Home.js
@@ -1,6 +1,3 @@
 exports.default = {
-  loggedInAs: "You are logged in as ",
-  cannotAccessFormMessage:
-    "You do not have permission to access this form. Please contact an\n" +
-    "            admin to get access.",
+  goToSubmissionText: "Go to submission page."
 };
