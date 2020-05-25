3f6c5c93173d2da721702957140cde3f815e38a0
Martin Staadecker <machstg@gmail.com>
2020-05-25 14:49:00 -0400

    Add hooks, install missing dependencies

--- src/volunteer/addVolunteerRoute.js
+++ src/volunteer/addVolunteerRoute.js
@@ -1,7 +1,10 @@
-const { addVolunteerAndAuthenticate } = require('./volunteerAPI');
+const { addVolunteerAndAuthenticate } = require("./volunteerAPI");
 
 module.exports = async (req, res) => {
   console.log(req);
-  let [success, message] = await addVolunteerAndAuthenticate(res.locals.volunteer, req.body.data);
-  res.send({success, message});
+  let [success, message] = await addVolunteerAndAuthenticate(
+    res.locals.volunteer,
+    req.body.data
+  );
+  res.send({ success, message });
 };
