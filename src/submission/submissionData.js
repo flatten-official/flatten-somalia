78c8d412e364a4a7e6418d63a4e2743b7d1ac7aa
Arthur Allshire <arthur@allshire.org>
2020-05-27 16:03:55 -0400

    update schema for submission data

--- src/submission/submissionData.js
+++ src/submission/submissionData.js
@@ -12,30 +12,30 @@ const Submission = mongoose.model(
     },
     // form schema version (the thing contained in the data object)
     schema: {
-      form: { type: String, index: true, required: true },
-      version: { type: String, index: true, required: true },
-    },
-    // raw submission data eg from formio
-    data: {
-      type: mongoose.Mixed,
-      required: true,
+      form: { type: String, index: true, required: true }, // eg. 'somaliaInitialVolunteerSurvey'
+      version: { type: String, index: true, required: true }, // eg. '1.0'
     },
     // contains location etc
-    meta: {
-      location: {
-        lat: Number,
-        long: Number,
-        accuracy: Number,
-        altitude: Number,
-      },
-      startTime: { type: Number, index: true },
-      endTime: { type: Number, index: true },
-      timeToComplete: Number,
-      consentGiven: Boolean,
-      timestamp: {
-        type: Date,
-        index: true,
-      },
+    location: {
+      lat: Number,
+      long: Number,
+      accuracy: Number,
+      altitude: Number,
+      wasManual: Boolean,
+    },
+    // recorded on the user's browser with JS Date.now()
+    filledOutTimestamp: { type: Number, index: true },
+    timeToComplete: Number, // ms
+    consentGiven: Boolean,
+    uploadTimestamp: {
+      type: Date,
+      index: true,
+    },
+    // this is filled in when this submission has been followed up with
+    // submission -> household
+    followUpId: {
+      type: mongoose.ObjectId,
+      required: false,
     },
   })
 );
@@ -43,19 +43,28 @@ const Submission = mongoose.model(
 const Household = mongoose.model(
   "Household",
   new mongoose.Schema({
-    // volunteer who made the submsision
-    addedBy: {
-      type: mongoose.ObjectId,
-      required: true,
-      index: true,
-    },
-    submissions: [{ type: mongoose.ObjectId, ref: "Submission" }],
+    submissions: [
+      {
+        // raw submission data (excluding people and death data)
+        data: {
+          type: mongoose.Mixed,
+        },
+        submissionRef: {
+          type: mongoose.ObjectId,
+          ref: "Submission",
+          index: true,
+        },
+      },
+    ],
+    // TODO - decide if we remove this
     latestPhone: String,
     latestEmail: String,
-    // the id that is given to volunteers (NOT the ID in the DB)
-    householdId: {
+    // the id that is given to volunteers (NOT the ID in the DB), TODO...!!
+    publicId: {
       type: String,
       required: true,
+      index: true,
+      unique: true,
     },
   })
 );
@@ -63,27 +72,25 @@ const Household = mongoose.model(
 const Person = mongoose.model(
   "Person",
   new mongoose.Schema({
-    submissions: [{ type: mongoose.ObjectId, ref: "Submission" }],
-    basicInfo: {
-      firstName: String,
-      lastName: String,
-      yearOfBirth: Number,
-      gender: {
-        type: String,
-        enum: [
-          // todo - what is the correct way to ask this stuff!
-          "male",
-          "female",
-          "preferNotToAnswer",
-          "unlisted",
-        ],
+    submissions: [
+      {
+        data: {
+          type: mongoose.Mixed,
+        },
+        submissionKind: {
+          type: String,
+          enum: ["person", "death"],
+        },
+        submissionRef: { type: mongoose.ObjectId, ref: "Submission" },
       },
-      isAlive: {
-        type: Boolean,
-        required: true,
-        index: true,
-        default: true,
-      },
+    ],
+    // todo - is there an easy way to query on the latest submission kind
+    // or if there is a kind that is death in the list
+    isAlive: {
+      type: Boolean,
+      required: true,
+      index: true,
+      default: true,
     },
     householdId: {
       type: mongoose.ObjectId,
@@ -92,18 +99,21 @@ const Person = mongoose.model(
   })
 );
 
-async function householdInitialSubmission(
-  volunteerId,
-  schema,
-  householdData,
-  peopleData,
-  deathData,
-  submissionMetadata
+// todo - is there a way to queue up the operations
+
+async function addSubmission(
+  submissionData, // break up into the sub components of a submission
+  previousId = undefined
 ) {}
 
-async function getDataByVolunteer(volunteerId) {}
+async function addHousehold(submissionData, submissionId) {}
 
-async function getHouseholdByHouseholdId(householdId) {}
+async function addPerson() {}
+
+async function addSubmissionToHousehold(householdId, newSubmission) {}
+async function addSubmissionToPerson(personId, newSubmission) {}
+
+
 
 module.exports = {
   householdInitialSubmission,
