01b032c08be16aa64a47f0481791edff390eb34d
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-05-27 23:17:54 -0400

    test missing, old, and valid token

--- test/auth/routes/verifyTokenRoute.test.js
+++ test/auth/routes/verifyTokenRoute.test.js
@@ -1,3 +1,6 @@
+const { signToken } = require("../../../src/utils/jwt");
+const { addVolunteer } = require("../../../src/volunteer/volunteerData");
+
 const { getApp } = require("../../../src/app");
 const util = require("../../testUtils/mongo");
 const supertest = require("supertest");
@@ -5,7 +8,7 @@ const { setup } = require("../../../src/index");
 
 let request;
 
-describe("test /auth/login", () => {
+describe("test /auth/token", () => {
   beforeAll(async () => {
     await setup(false);
     await util.connectToDatabase();
@@ -17,4 +20,28 @@ describe("test /auth/login", () => {
     jest.clearAllMocks();
   });
   afterAll(async () => await util.closeDatabase());
+
+  it("should return 400 if token is absent", async () => {
+    const res = await request.get("/auth/token");
+    expect(res.status).toBe(400);
+  });
+
+  it("should fail with 401 for an old token", async () => {
+    // token used here was signed for a valid email on May 27 and should be expired
+    const res = await request.get(
+      "/auth/token?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlY2ViZDI0NTE5ZjIyMjY4NDVkNjRiMCIsImlhdCI6MTU5MDYwODY3NiwiZXhwIjoxNTkxNTA4Njc2fQ.9XsbPNiybwkZl2M7v3orXk_Imn66vvGflRG267MJ6Ds"
+    );
+    expect(res.status).toBe(401);
+  });
+
+  it("should return a cookie & redirect for a valid token", async () => {
+    let volunteerId = await addVolunteer("Kanye West", "good@gmail.com", null);
+    let token = await signToken({ id: volunteerId }, 10);
+
+    let res = await request.get("/auth/token?token=" + token);
+    console.log(!!res);
+    // cookie should exist
+    expect(!!res.cookie).toBe(true);
+    expect(res.status).toBe(303);
+  });
 });
