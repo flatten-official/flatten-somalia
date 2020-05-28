2d4b4a120a2ca8f1edb107cdcb752d01405906fa
Arthur Allshire <arthur@allshire.org>
2020-05-28 11:27:35 -0400

    test adding extra submission to a household

--- test/submission/submissionData.test.js
+++ test/submission/submissionData.test.js
@@ -5,24 +5,43 @@ const { setup } = require("../../src/index");
 const mongoose = require("mongoose");
 
 const dummyVolunteerId = "56cb91bdc3464f14678934ca";
-const basicSubmission = {
-  addedBy: mongoose.Types.ObjectId(dummyVolunteerId),
-  submissionSchema: {
-    form: "addVolunteerForm",
-    version: "1.0",
+const testSubmissions = [
+  {
+    addedBy: mongoose.Types.ObjectId(dummyVolunteerId),
+    submissionSchema: {
+      form: "householdInitialSubmission",
+      version: "1.0",
+    },
+    location: {
+      lat: 0,
+      long: 0,
+    },
+    filledOutTimestamp: Date.now(),
+    timeToComplete: 3000,
+    consentGiven: true,
   },
-  location: {
-    lat: 0,
-    long: 0,
+  {
+    addedBy: mongoose.Types.ObjectId(dummyVolunteerId),
+    submissionSchema: {
+      form: "householdFollowupSubmission",
+      version: "1.0",
+    },
+    location: {
+      lat: 50,
+      long: 50,
+    },
+    filledOutTimestamp: Date.now(),
+    timeToComplete: 2000,
+    consentGiven: true,
   },
-  filledOutTimestamp: Date.now(),
-  timeToComplete: 3000,
-  consentGiven: true,
-};
+];
 const basicHouseholdId = "id-test-0";
-const basicHouseholdData = {
+const initialHouseholdData = {
   email: "test@example.com",
   phone: "01189998819991197253",
+};
+const followupHouseholdData = {
+  awareOfCovidHotline: true,
 };
 
 const basicPeople = [
@@ -47,12 +66,12 @@ describe("submission database functions", () => {
 
   it("should write submission to database", async () => {
     const submissionId = await submissionData.addSubmission(
-      basicSubmission.addedBy,
-      basicSubmission.submissionSchema,
-      basicSubmission.location,
-      basicSubmission.filledOutTimestamp,
-      basicSubmission.timeToComplete,
-      basicSubmission.consentGiven
+      testSubmissions[0].addedBy,
+      testSubmissions[0].submissionSchema,
+      testSubmissions[0].location,
+      testSubmissions[0].filledOutTimestamp,
+      testSubmissions[0].timeToComplete,
+      testSubmissions[0].consentGiven
     );
 
     const all = await submissionData.Submission.find();
@@ -61,36 +80,38 @@ describe("submission database functions", () => {
     const retrievedSubmission = all[0];
 
     expect(retrievedSubmission._id).toStrictEqual(submissionId);
-    expect(basicSubmission.addedBy).toStrictEqual(retrievedSubmission.addedBy);
-    expect(basicSubmission.submissionSchema.form).toStrictEqual(
+    expect(testSubmissions[0].addedBy).toStrictEqual(
+      retrievedSubmission.addedBy
+    );
+    expect(testSubmissions[0].submissionSchema.form).toStrictEqual(
       retrievedSubmission.submissionSchema.form
     );
-    expect(basicSubmission.submissionSchema.version).toStrictEqual(
+    expect(testSubmissions[0].submissionSchema.version).toStrictEqual(
       retrievedSubmission.submissionSchema.version
     );
-    expect(basicSubmission.filledOutTimestamp).toStrictEqual(
+    expect(testSubmissions[0].filledOutTimestamp).toStrictEqual(
       retrievedSubmission.filledOutTimestamp
     );
-    expect(basicSubmission.timeToComplete).toStrictEqual(
+    expect(testSubmissions[0].timeToComplete).toStrictEqual(
       retrievedSubmission.timeToComplete
     );
-    expect(basicSubmission.consentGiven).toStrictEqual(
+    expect(testSubmissions[0].consentGiven).toStrictEqual(
       retrievedSubmission.consentGiven
     );
   });
 
   it("should create a household correctly", async () => {
     const submissionId = await submissionData.addSubmission(
-      basicSubmission.addedBy,
-      basicSubmission.submissionSchema,
-      basicSubmission.location,
-      basicSubmission.filledOutTimestamp,
-      basicSubmission.timeToComplete,
-      basicSubmission.consentGiven
+      testSubmissions[0].addedBy,
+      testSubmissions[0].submissionSchema,
+      testSubmissions[0].location,
+      testSubmissions[0].filledOutTimestamp,
+      testSubmissions[0].timeToComplete,
+      testSubmissions[0].consentGiven
     );
 
     const householdId = await submissionData.createHousehold(
-      basicHouseholdData,
+      initialHouseholdData,
       basicHouseholdId,
       submissionId
     );
@@ -103,10 +124,10 @@ describe("submission database functions", () => {
     expect(retrievedHousehold._id).toStrictEqual(householdId);
     expect(retrievedHousehold.publicId).toStrictEqual(basicHouseholdId);
     expect(retrievedHousehold.submissions[0].data.phone).toStrictEqual(
-      basicHouseholdData.phone
+      initialHouseholdData.phone
     );
     expect(retrievedHousehold.submissions[0].data.email).toStrictEqual(
-      basicHouseholdData.email
+      initialHouseholdData.email
     );
     expect(retrievedHousehold.submissions[0].submissionRef).toStrictEqual(
       submissionId
@@ -115,16 +136,16 @@ describe("submission database functions", () => {
 
   it("should create a person within a household correctly", async () => {
     const submissionId = await submissionData.addSubmission(
-      basicSubmission.addedBy,
-      basicSubmission.submissionSchema,
-      basicSubmission.location,
-      basicSubmission.filledOutTimestamp,
-      basicSubmission.timeToComplete,
-      basicSubmission.consentGiven
+      testSubmissions[0].addedBy,
+      testSubmissions[0].submissionSchema,
+      testSubmissions[0].location,
+      testSubmissions[0].filledOutTimestamp,
+      testSubmissions[0].timeToComplete,
+      testSubmissions[0].consentGiven
     );
 
     const householdId = await submissionData.createHousehold(
-      basicHouseholdData,
+      initialHouseholdData,
       basicHouseholdId,
       submissionId
     );
@@ -152,17 +173,69 @@ describe("submission database functions", () => {
       let personDb = all.filter(
         (obj) => obj["_id"].toString() === id.toString()
       )[0];
-      console.log(personDb);
-      console.log(personDb.submissions[0]);
       expect(personDb.submissions[0].data).toStrictEqual(basicPeople[i].data);
       expect(personDb.submissions[0].submissionKind).toStrictEqual(
         basicPeople[i].submissionKind
       );
-      expect(personDb.submissions[0].submissionRef.toString()).toStrictEqual(submissionId.toString());
+      expect(personDb.submissions[0].submissionRef.toString()).toStrictEqual(
+        submissionId.toString()
+      );
     }
   });
 
-  it("should add a submission to a household correctly", async () => {});
+  it("should add a submission to a household correctly", async () => {
+    let submissionIds = [];
+    for (let submission of testSubmissions) {
+      submissionIds.push(
+        await submissionData.addSubmission(
+          submission.addedBy,
+          submission.submissionSchema,
+          submission.location,
+          submission.filledOutTimestamp,
+          submission.timeToComplete,
+          submission.consentGiven
+        )
+      );
+    }
+
+    const householdId = await submissionData.createHousehold(
+      initialHouseholdData,
+      basicHouseholdId,
+      submissionIds[0]
+    );
+
+    await submissionData.addSubmissionToHousehold(
+      householdId,
+      followupHouseholdData,
+      submissionIds[1]
+    );
+
+    const submissions = await submissionData.Submission.find();
+    expect(submissions).toHaveLength(2);
+
+    const all = await submissionData.Household.find();
+    expect(all).toHaveLength(1);
+
+    const retrievedHousehold = all[0];
+
+    expect(retrievedHousehold._id).toStrictEqual(householdId);
+    expect(retrievedHousehold.publicId).toStrictEqual(basicHouseholdId);
+    expect(retrievedHousehold.submissions[0].data.phone).toStrictEqual(
+      initialHouseholdData.phone
+    );
+    expect(retrievedHousehold.submissions[0].data.email).toStrictEqual(
+      initialHouseholdData.email
+    );
+    expect(retrievedHousehold.submissions[0].submissionRef).toStrictEqual(
+      submissionIds[0]
+    );
+    expect(
+      retrievedHousehold.submissions[1].data.awareOfCovidHotline
+    ).toStrictEqual(followupHouseholdData.awareOfCovidHotline);
+    expect(retrievedHousehold.submissions[1].submissionRef).toStrictEqual(
+      submissionIds[1]
+    );
+  });
 
   it("should add a submission to a person correctly", async () => {});
 });
