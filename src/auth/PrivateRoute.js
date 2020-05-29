e59bbe0a8f4b31e2f4e762956b98797760190665
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-05-29 02:33:26 -0400

    localize unauthorized message

--- src/auth/PrivateRoute.js
+++ src/auth/PrivateRoute.js
@@ -4,13 +4,15 @@ import { hasPermission } from "./authApi";
 import { Routes } from "../config";
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useTranslation } from "react-i18next";
 
 function PrivateRoute({ requiredPermission, comp: Component, ...rest }) {
   const auth = useSelector((state) => state.auth);
   const authed = hasPermission(auth, requiredPermission);
+  const { t } = useTranslation()
   // if logged in but without the right permission to use this page
   if (!authed && auth.state === AUTH_SUCCESS) {
-    return <div>Unauthorised.</div>;
+    return <div>{t('Admin:unauthorized')}</div>;
   }
   return (
     <Route
