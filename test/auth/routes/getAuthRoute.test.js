a5f80ee9d05aea47b7b541d87156a5dcede159bf
Martin Staadecker <machstg@gmail.com>
2020-05-29 23:01:00 -0400

    Fix getAuthRoute tests

--- test/auth/routes/getAuthRoute.test.js
+++ test/auth/routes/getAuthRoute.test.js
@@ -1,51 +1,42 @@
-const { signToken } = require("../../../src/utils/jwt");
-const { addVolunteer } = require("../../../src/volunteer/volunteerData");
-
 const { getApp } = require("../../../src/app");
 const util = require("../../testUtils/mongo");
 const supertest = require("supertest");
 const { setup } = require("../../../src/index");
 
+const { login } = require("../../testUtils/requests");
+
 let request;
+let app;
 
 describe("test /auth", () => {
   beforeAll(async () => {
     await setup(false);
     await util.connectToDatabase();
-    request = supertest(await getApp());
+    app = await getApp();
+    request = supertest(app);
   });
 
-  afterEach(async () => {
-    await util.clearDatabase();
-    jest.clearAllMocks();
-  });
+  afterEach(async () => await util.clearDatabase());
   afterAll(async () => await util.closeDatabase());
 
   it("should return an empty object when no cookie is provided", async () => {
-    let res = await request.get("/auth");
+    const res = await request.get("/auth");
 
     expect(res.status).toBe(200);
     // object should be empty
-    expect(res.body.constructor).toBe(Object);
-    expect(Object.keys(res.body)).toHaveLength(0);
+    expect(res.body).toStrictEqual({});
   });
 
   it("should return  name, permissions, and session expiry for a logged in volunteer", async () => {
-    const name = "big poppa";
-    const permissions = ["submitForms"];
+    const { agent, volunteerName } = await login(app);
 
-    let volunteerId = await addVolunteer(name, "irrelevant@aladin.ca", null);
-    let token = await signToken({ id: volunteerId }, 10);
-    let loginRes = await request.get("/auth/token?token=" + token);
+    const res = await agent.get("/auth");
 
-    let res = await request
-      .get("/auth")
-      .set("cookie", loginRes.headers["set-cookie"]);
-
-    expect(res.body["name"]).toBe(name);
+    expect(res.status).toBe(200);
+    expect(res.body.name).toBe(volunteerName);
     // expected value set in volunteerData.js
-    expect(res.body["permissions"]).toMatchObject(permissions);
+    expect(res.body.permissions).toMatchObject(["submitForms"]);
     // expiry field should exist
-    expect(res.body["expiry"]).toBe(true);
+    expect(res.body.expiry).not.toBeNull();
   });
 });
---
+++ test/testUtils/requests.js
@@ -0,0 +1,22 @@
+const { addVolunteer } = require("../../src/volunteer/volunteerData");
+const { signToken } = require("../../src/utils/jwt");
+const supertest = require("supertest");
+
+/**
+ * Most importantly returns an agent that stores cookies and can be used to call other endpoints with cookies
+ */
+module.exports.login = async (
+  app,
+  volunteerName = "name",
+  volunteerEmail = "test@example.ca"
+) => {
+  const agent = supertest.agent(app, {});
+
+  const volunteerId = await addVolunteer(volunteerName, volunteerEmail, null);
+
+  const token = await signToken({ id: volunteerId }, 10);
+
+  await agent.get("/auth/token?token=" + token);
+
+  return { agent, volunteerName, volunteerEmail };
+};
