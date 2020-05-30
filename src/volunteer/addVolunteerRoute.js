68c751e93acf093a6397c02351e7e52f1a05980d
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-05-29 21:07:12 -0400

    comments to clear up params when adding volunteer

--- src/volunteer/addVolunteerRoute.js
+++ src/volunteer/addVolunteerRoute.js
@@ -2,7 +2,9 @@ const { addVolunteerAndAuthenticate } = require("./volunteerAPI");
 
 module.exports = async (req, res) => {
   let [success, message] = await addVolunteerAndAuthenticate(
+    // admin doing the adding
     res.locals.volunteer,
+    // volunteer to be added
     req.body.volunteerData
   );
   res.send({ success, message });
