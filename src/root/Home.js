1c8d7725234800f8f6192df4baeb6f95c27c79f5
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-05-31 17:39:39 -0400

    tweak spacing

--- src/root/Home.js
+++ src/root/Home.js
@@ -15,8 +15,9 @@ const Home = () => {
         <div className="websitetitle">
           <b>{t("Home:welcome")}</b>
         </div>
+        <div className="fourtypxmargin" />
         <div className="heading">{t("Home:formSelectionPrompt")}</div>
-        <div className="fourtypxmargin"></div>
+        <div className="twentypxmargin" />
         <div className="buttons">
           {/*followUp && <div className="alertText">Home:followUpsText</div> */}
           <Link to={Routes.submission}>{t("Home:goToSubmissionText")}</Link>
@@ -25,9 +26,9 @@ const Home = () => {
           <Link to={null}>{t("Home:goToGraveDigger")}</Link>
         </div>
         <div className="buttons">
-          {/*This is filler, there should be a check for if there are follow ups and a notification will show*/}
+          {/*This is filler, TODO there should be a check for if there are follow ups and a notification will show*/}
           <Link to={null}>{t("Home:goToFollowUp")}</Link>
-          {/* Don't know if theres a more secure/better way todo this auth.user.permissions.includes(
+          {/* Don't know if theres a more secure/better way to do this auth.user.permissions.includes(
             "manageVolunteers"
           ) && <Link to={null}>{t("Home:goToAddVolunteer")}</Link> */}
         </div>
