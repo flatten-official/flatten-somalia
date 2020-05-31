776a68b1ca900b3bb974a75cf5611720a5871df4
Arthur Allshire <arthur@allshire.org>
2020-05-31 09:07:24 -0400

    basic tests that submission works + rejects as desired

--- test/submission/routes/submitRoutes.test.js
+++ test/submission/routes/submitRoutes.test.js
@@ -13,6 +13,35 @@ const retrieveById = (all, id) =>
 let request;
 let app;
 
+const sampleSubmission = {
+  household: {
+    someHouseholdData: "foo",
+    publicId: "bar-007",
+  },
+  people: [{ name: "personA" }, { name: "personB" }],
+  deaths: [{ name: "personC" }],
+  metadata: { filledOutTimestamp: Date.now(), consentGiven: true },
+  schema: {
+    form: "volunteerInitialForm",
+    version: "0.1",
+  },
+};
+
+const sampleSubmissionInvalid = {
+  household: {
+    someHouseholdData: "foo",
+    publicId: "bar-007",
+  },
+  people: [{ name: "personA" }, { name: "personB" }],
+  deaths: [{ name: "personC" }],
+  // no consent given -> invalid
+  metadata: { filledOutTimestamp: Date.now() },
+  schema: {
+    form: "volunteerInitialForm",
+    version: "0.1",
+  },
+};
+
 describe("test /auth", () => {
   beforeAll(async () => {
     await setup(false);
@@ -29,20 +58,6 @@ describe("test /auth", () => {
 
   it("should add submissions, people, and households as expected for an initial submission", async () => {
     const { agent, volunteer } = await login(app);
-
-    const sampleSubmission = {
-      household: {
-        someHouseholdData: "foo",
-        publicId: "bar-007",
-      },
-      people: [{ name: "personA" }, { name: "personB" }],
-      deaths: [{ name: "personC" }],
-      metadata: { filledOutTimestamp: Date.now() },
-      schema: {
-        form: "volunteerInitialForm",
-        version: "0.1",
-      },
-    };
 
     const res = await agent
       .post("/submit/initial")
@@ -62,17 +77,49 @@ describe("test /auth", () => {
     const submission = allSubmissions[0];
     const household = allHouseholds[0];
 
-    expect(submission.household.ref).toStrictEqual(household.ref);
+    expect(submission.household.ref).toStrictEqual(household._id);
     expect(submission.household.data).toStrictEqual(sampleSubmission.household);
 
     expect(household.publicId).toStrictEqual(
       sampleSubmission.household.publicId
     );
+  });
 
-    // todo - check all people in the database
+  it("should fail for a user without the right permissions", async () => {
+    const { agent, volunteer } = await login(app, { permissions: [] });
 
+    const res = await agent
+      .post("/submit/initial")
+      .send(sampleSubmission)
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .expect(403);
 
-    // TODO
-    expect(true).toStrictEqual(false);
+    const allSubmissions = await submissionData.Submission.find();
+    const allHouseholds = await submissionData.Household.find();
+    const allPeople = await submissionData.Person.find();
+
+    expect(allSubmissions).toHaveLength(0);
+    expect(allHouseholds).toHaveLength(0);
+    expect(allPeople).toHaveLength(0);
+  });
+
+  it("should fail for an invalid submission", async () => {
+    const { agent, volunteer } = await login(app);
+
+    const res = await agent
+      .post("/submit/initial")
+      .send(sampleSubmissionInvalid)
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .expect(400);
+
+    const allSubmissions = await submissionData.Submission.find();
+    const allHouseholds = await submissionData.Household.find();
+    const allPeople = await submissionData.Person.find();
+
+    expect(allSubmissions).toHaveLength(0);
+    expect(allHouseholds).toHaveLength(0);
+    expect(allPeople).toHaveLength(0);
   });
 });
