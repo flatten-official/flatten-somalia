edd062c5d4e5dc6e35649cee04f32186119acc54
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-05-31 18:47:40 -0400

    switch to using React buttons

--- src/root/Home.js
+++ src/root/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { Routes } from "../config";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
   //let auth = useSelector((state) => state.auth); // use the auth state from the store
@@ -18,20 +19,19 @@ const Home = () => {
         <div className="fourtypxmargin" />
         <div className="heading">{t("Home:formSelectionPrompt")}</div>
         <div className="twentypxmargin" />
-        <div className="buttons">
-          {/*followUp && <div className="alertText">Home:followUpsText</div> */}
-          <Link to={Routes.submission}>{t("Home:goToSubmissionText")}</Link>
-        </div>
-        <div className="buttons">
-          <Link to={null}>{t("Home:goToGraveDigger")}</Link>
-        </div>
-        <div className="buttons">
-          {/*This is filler, TODO there should be a check for if there are follow ups and a notification will show*/}
-          <Link to={null}>{t("Home:goToFollowUp")}</Link>
-          {/* Don't know if theres a more secure/better way to do this auth.user.permissions.includes(
+        <Button variant="light" size="lg" href={Routes.submission}>
+          {t("Home:goToInitialSurvey")}
+        </Button>
+        <Button variant="light" size="lg" href={null}>
+          {t("Home:goToGravediggerSurvey")}
+        </Button>
+        {/*TODO there should be a check for if there are follow ups and a notification will show*/}
+        <Button variant="light" size="lg" href={null}>
+          {t("Home:goToFollowupSurvey")}
+        </Button>
+        {/* Don't know if theres a more secure/better way to do this auth.user.permissions.includes(
             "manageVolunteers"
           ) && <Link to={null}>{t("Home:goToAddVolunteer")}</Link> */}
-        </div>
       </center>
     </>
   );
