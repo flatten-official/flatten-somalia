c10cae878a01e066f19e9c7f399ce6e5c8da4b07
Martin Staadecker <machstg@gmail.com>
2020-05-31 16:53:37 -0400

    Fix unauthenticated permission not crashing server

--- src/submission/submissionMiddleware.js
+++ src/submission/submissionMiddleware.js
@@ -1,7 +1,10 @@
 const volunteerAPI = require("../volunteer/volunteerAPI");
 
 module.exports = async (req, res, next) => {
-  if (volunteerAPI.canSubmitForms(res.locals.volunteer)) {
+  if (
+    res.locals.volunteer &&
+    volunteerAPI.canSubmitForms(res.locals.volunteer)
+  ) {
     next();
   } else {
     res.sendStatus(403);
