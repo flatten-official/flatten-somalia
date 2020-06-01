19e32d4b644f632181ecfe2024cb4204690cdbc2
Martin Staadecker <machstg@gmail.com>
2020-06-01 01:16:28 -0400

    Add follow id to submission

--- src/form/SubmissionPage.js
+++ src/form/SubmissionPage.js
@@ -22,7 +22,11 @@ const SubmissionPageContent = () => {
   const onSubmit = async (data) => {
     const endTime = Date.now();
     const reformatted = {
-      household: {},
+      household: {
+        followUpId: formData.followUpId,
+      },
+      people: data.personGrid,
+      deaths: data.deathGrid,
       metadata: {
         endTime: endTime,
         timeToComplete: endTime - formData.startTime,
@@ -34,20 +38,18 @@ const SubmissionPageContent = () => {
         version: "0.1",
       },
     };
-    Object.entries(data).map(([k, v]) => {
-      if (k === "personGrid") {
-        reformatted.people = v;
-      } else if (k === "deathGrid") {
-        reformatted.deaths = v;
-      } else {
+
+    Object.entries(data).forEach(([k, v]) => {
+      if (!(k === "personGrid" || k === "deathGrid"))
         reformatted.household[k] = v;
-      }
     });
+
     // need to actually add the submission in here!
     await backend.request({
       ...flattenApi.volunteerForm,
       data: reformatted,
     });
+
     dispatch(push("/success"));
   };
 
