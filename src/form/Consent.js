a09def08a71499c77b21e66a2a222a3e1c6772d5
Martin Staadecker <machstg@gmail.com>
2020-06-01 03:08:04 -0400

    Switch to follow up id being inside form

--- src/form/Consent.js
+++ src/form/Consent.js
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next";
-import { useDispatch } from "react-redux";
-import { Types } from "./FormActions";
+import { useDispatch, useSelector } from "react-redux";
+import { setFollowUpId, Types } from "./FormActions";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import React from "react";
 
@@ -8,9 +8,14 @@ export const Consent = () => {
   const { t } = useTranslation("VolunteerForm");
   const dispatch = useDispatch();
 
+  const volunteerFriendlyId = useSelector(
+    (state) => state.auth.user.friendlyId
+  );
+
   const onConsent = () => {
     dispatch({ type: Types.NOTIFY_CONSENT_GIVEN });
     dispatch({ type: Types.SET_START_TIME, payload: Date.now() });
+    dispatch(setFollowUpId(volunteerFriendlyId));
   };
 
   return (
