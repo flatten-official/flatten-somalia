9578352620900d6fce00dd26d72ae117dad1d73a
Martin Staadecker <machstg@gmail.com>
2020-06-02 02:21:35 -0400

    Cleanup submission

--- src/ui/initialSurvey/SubmissionPage.js
+++ src/ui/initialSurvey/SubmissionPage.js
@@ -5,10 +5,9 @@ import { Types } from "./actions";
 import { LocationPicker } from "./elements/location/LocationPicker";
 import Form from "../components/formio/Form";
 import FormDef from "../../forms/VolunteerForm.json";
-import flattenApi from "../../backend/api/api";
-import backend from "../../backend/api/backend";
 import { push } from "connected-react-router";
 import { Consent } from "./elements/Consent";
+import { submitForm } from "../../backend/submission";
 
 const SubmissionPageContent = () => {
   const formData = useSelector((state) => state.volunteerForm);
@@ -18,48 +17,19 @@ const SubmissionPageContent = () => {
     dispatch({ type: Types.RESTART_FORM });
   }, [dispatch]);
 
-  const onSubmit = async (data) => {
-    const endTime = Date.now();
-    const reformatted = {
-      household: {
-        followUpId: formData.followUpId,
-      },
-      people: data.personGrid,
-      deaths: data.deathGrid,
-      metadata: {
-        endTime: endTime,
-        timeToComplete: endTime - formData.startTime,
-        location: formData.location,
-        consentGiven: formData.consent,
-      },
-      schema: {
-        form: "volunteerInitialForm",
-        version: "0.1",
-      },
-    };
-
-    Object.entries(data).forEach(([k, v]) => {
-      if (!(k === "personGrid" || k === "deathGrid"))
-        reformatted.household[k] = v;
-    });
-
-    // need to actually add the submission in here!
-    await backend.request({
-      ...flattenApi.volunteerForm,
-      data: reformatted,
-    });
-
-    dispatch(push("/success"));
-  };
-
   if (!formData.consent) return <Consent />;
 
   if (!formData.location) return <LocationPicker />;
 
+  const submitHook = async (formioData) => {
+    await submitForm(formData, formioData);
+    dispatch(push("/success"));
+  };
+
   return (
     <Form
       formioForm={FormDef}
-      submitHook={onSubmit}
+      submitHook={submitHook}
       formioOptions={{ noAlerts: false }}
     />
   );
@@ -69,10 +39,10 @@ const SubmissionPage = () => {
   const { t } = useTranslation("InitialSurvey");
 
   return (
-    <div>
+    <>
       <h3> {t("title")} </h3>
       <SubmissionPageContent />
-    </div>
+    </>
   );
 };
 
