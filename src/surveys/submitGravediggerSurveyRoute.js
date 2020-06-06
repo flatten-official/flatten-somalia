71715a22ca1754f9e2157a33227bb38e7c14d695
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-06-06 01:04:54 -0400

    implement gravedigger survey submission route

--- src/surveys/submitGravediggerSurveyRoute.js
+++ src/surveys/submitGravediggerSurveyRoute.js
@@ -1,3 +1,20 @@
+const submissionAPI = require("../surveySubmissions/submissionAPI");
+
 module.exports = async (req, res) => {
-  res.sendStatus(500);
+  try {
+    if (req.body.perDeath === undefined) req.body.perDeath = [];
+    await submissionAPI.gravediggerSurveySubmission(
+      res.locals.volunteer._id,
+      res.locals.volunteer.teamName,
+      req.body.schema,
+      req.body.metadata,
+      req.body.data
+    );
+    res.sendStatus(200);
+  } catch (e) {
+    if (e instanceof Error.ValidationError) {
+      console.error(e);
+      res.status(400).send("Validation problem with form data.");
+    } else throw e;
+  }
 };
