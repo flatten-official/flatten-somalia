52f3139c32563b8ec32d26f983a8cfd4ebcda6f8
Martin Staadecker <machstg@gmail.com>
2020-06-15 12:08:15 -0400

    Fix the warning bug and inline success page

--- src/ui/components/surveys/Success.js
+++ src/ui/components/surveys/Success.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { Routes } from "../../config";
+import { Routes } from "../../../config";
 
 const Success = () => {
   const { t } = useTranslation("Surveys");
   return (
-    <>
+    <h4>
       {t("success")}
       <Link to={Routes.home}> {t("returnHomePrompt")} </Link>
-    </>
+    </h4>
   );
 };
 
