0712b33e373b5272ee83166ab77d07ec1c28c373
Martin Staadecker <machstg@gmail.com>
2020-06-19 14:15:00 -0400

    Simplify submissionRoute.js

--- src/surveys/initialHousehold/submissionRoute.js
+++ src/surveys/initialHousehold/submissionRoute.js
@@ -1,16 +1,13 @@
 const { initialSubmission } = require("./api");
 
 module.exports = async (req, res) => {
-  if (req.body.people === undefined) req.body.people = [];
-  if (req.body.deaths === undefined) req.body.deaths = [];
-
   await initialSubmission(
     res.locals.volunteer._id,
     res.locals.volunteer.teamName,
     req.body.schema,
     req.body.metadata,
-    req.body.people,
-    req.body.deaths,
+    req.body.people || [],
+    req.body.deaths || [],
     req.body.household
   );
 
