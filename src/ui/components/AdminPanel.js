2fc849d7887b41df7ef91546c597ad7beb1a3f00
Arthur Allshire <arthur@allshire.org>
2020-06-26 16:08:40 -0400

    add enable / disable buttons (don't send things yet)

--- src/ui/components/AdminPanel.js
+++ src/ui/components/AdminPanel.js
@@ -6,8 +6,10 @@ import {
   FETCH_LIST_PENDING,
   VOLUNTEER_CHANGE_FAILED,
 } from "../../backend/volunteer/volunteerActions";
+import { Button } from "react-bootstrap";
 import BootstrapTable from "react-bootstrap-table-next";
-
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Loading from "./Loading";
 
 const AdminPanel = () => {
@@ -17,9 +19,37 @@ const AdminPanel = () => {
   }, [dispatch]);
   const volunteer = useSelector((state) => state.volunteer);
 
+  const hasAccessFormatter = (row, cell) => {
+    const enabled = cell.permissions.indexOf("access") !== -1;
+    return <FontAwesomeIcon icon={enabled ? faCheck : faTimes} />;
+  };
+
+  const accessButtonFormatter = (row, cell) => {
+    const enabled = cell.permissions.indexOf("access") !== -1;
+    return (
+      <Button variant={enabled ? "warning" : "primary"}>
+        {enabled ? "Disable" : "Enable"}
+      </Button>
+    );
+  };
+
   const columns = [
     { dataField: "name", text: "Volunteer Name" },
     { dataField: "email", text: "Volunteer Email" },
+    {
+      dataField: "hasAccess",
+      text: "Volunteer Status",
+      isDummyField: true,
+      csvExport: false,
+      formatter: hasAccessFormatter,
+    },
+    {
+      dataField: "accessButton",
+      text: "Enable/Disable Volunteer",
+      isDummyField: true,
+      csvExport: false,
+      formatter: accessButtonFormatter,
+    },
   ];
 
   if (volunteer.listStatus === FETCH_LIST_PENDING) return <Loading />;
