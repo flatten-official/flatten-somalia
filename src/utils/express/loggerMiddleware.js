4fe83e976249be1f5bb4995391a3805cf92a7ac5
Martin Staadecker <machstg@gmail.com>
2020-06-29 13:19:47 -0400

    Downgrade to debug middleware

--- src/utils/express/loggerMiddleware.js
+++ src/utils/express/loggerMiddleware.js
@@ -1,6 +1,6 @@
 const { log } = require("../../utils/winston");
 
 module.exports = (req, res, next) => {
-  log.info(`Endpoint hit: `, { method: req.method, path: req.path });
+  log.debug(`Endpoint hit: `, { method: req.method, path: req.path });
   next();
 };
