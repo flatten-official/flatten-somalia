62be32f1e3932135e3c3f2a9ba27b463430f6630
Martin Staadecker <machstg@gmail.com>
2020-06-29 14:47:28 -0400

    Add forward slash to in logger to clarify when root is hit

--- src/utils/express/loggerMiddleware.js
+++ src/utils/express/loggerMiddleware.js
@@ -1,6 +1,6 @@
 const { log } = require("../../utils/winston");
 
 module.exports = (req, res, next) => {
-  log.debug(`Endpoint hit: `, { method: req.method, path: req.path });
+  log.debug(`Endpoint hit: /`, { method: req.method, path: req.path });
   next();
 };
