eac63713f77d516108e851e25978ecdba6d6561c
Martin Staadecker <machstg@gmail.com>
2020-07-20 18:45:59 -0400

    Improve way errors are handled
    Signed-off-by: Martin Staadecker <machstg@gmail.com>

--- packages/backend/src/launch.js
+++ packages/backend/src/launch.js
@@ -1,10 +1,11 @@
 if (process.env.ENVIRONMENT === "dev") require("dotenv").config();
 
 const { setup, startServer, cleanup } = require("./index");
+const { log } = require("util-logging");
 
 setup({ database: true, sendGrid: true })
-  .then(() => startServer())
+  .then(startServer)
   .catch(async (e) => {
+    log.emergency("Server crashed.", { error: e });
     await cleanup();
-    throw e;
   });
