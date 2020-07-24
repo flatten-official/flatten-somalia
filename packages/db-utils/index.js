91328b67cd1f2ab887363a78171c650f2079404d
Martin Staadecker <machstg@gmail.com>
2020-07-24 12:41:51 -0400

    Inline connection options
    Signed-off-by: Martin Staadecker <machstg@gmail.com>

--- packages/db-utils/index.js
+++ packages/db-utils/index.js
@@ -1,4 +1,9 @@
 module.exports = {
   mongoose: require("mongoose"),
   useReplicaSet: process.env.DISABLE_TRANSACTIONS !== "true",
+  connectionOptions: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
 };
