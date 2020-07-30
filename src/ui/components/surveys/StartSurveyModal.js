f5edaae8d1a9893ac6d80963c1379113521fc2a1
Martin Staadecker <machstg@gmail.com>
2020-07-30 11:26:23 -0400

    Add expire soon modal to start page with lower time

--- src/ui/components/surveys/StartSurveyModal.js
+++ src/ui/components/surveys/StartSurveyModal.js
@@ -2,12 +2,14 @@ import { useTranslation } from "react-i18next";
 import { Button } from "react-bootstrap";
 import React from "react";
 import PropTypes from "prop-types";
+import { ExpireSoonModal } from "../Modal";
 
 export const StartSurveyModal = ({ onStartSurvey }) => {
   const { t } = useTranslation("Surveys");
 
   return (
     <>
+      <ExpireSoonModal minutes={30} />
       <h4>{t("startSurveyPreamble")}</h4>
       <br />
       <Button variant="light" size="lg" onClick={onStartSurvey}>
