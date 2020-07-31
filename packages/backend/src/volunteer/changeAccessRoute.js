297054297045944844a09947a0094fe30ad476a2
Martin Staadecker <machstg@gmail.com>
2020-07-31 13:02:54 -0400

    Improve TODO
    Signed-off-by: Martin Staadecker <machstg@gmail.com>

--- packages/backend/src/volunteer/changeAccessRoute.js
+++ packages/backend/src/volunteer/changeAccessRoute.js
@@ -1,7 +1,7 @@
 const { changeVolunteerAccessById } = require("./volunteerAPI");
 
 module.exports = async (req, res) => {
-  // TODO ADD validation to body, also read the two params in the route instead of the api
+  // TODO ADD validation to body
   const result = await changeVolunteerAccessById(
     res.locals.volunteer,
     res.body.volunteerId,
