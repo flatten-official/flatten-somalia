2fd318475a34e51b074b9aa90c7ca65e1edaa27d
Martin Staadecker <machstg@gmail.com>
2020-08-04 15:24:11 -0400

    Remove log statements

--- src/backend/volunteer/volunteerReducer.js
+++ src/backend/volunteer/volunteerReducer.js
@@ -12,11 +12,6 @@ import update from "immutability-helper";
  * Updates the state of a single volunteer in the list
  */
 const updateVolunteerStatusById = (state, action) => {
-  console.log(action);
-  // todo - handle errors
-  if (!action.payload || !action.payload._id)
-    throw new Error("No id specified in VOLUNTEER_CHANGE action payload");
-
   const updateIndex = state.list.findIndex((v) => v._id === action.payload._id);
 
   // set the status
@@ -25,8 +20,6 @@ const updateVolunteerStatusById = (state, action) => {
   // set permissions if applicable
   if (action.payload.permissions)
     operation.permissions = { $set: action.payload.permissions };
-
-  console.log(operation);
 
   // returned the mutated state using the operation
   return update(state, { list: { [updateIndex]: operation } });
