d1403ef94c57d60fc7cf9f5f42287eedaec212ec
Martin Staadecker <machstg@gmail.com>
2020-08-21 17:36:06 -0400

    Make comment about mapping
    Signed-off-by: Martin Staadecker <machstg@gmail.com>

--- packages/backend/src/surveys/surveyRouteSplitter.js
+++ packages/backend/src/surveys/surveyRouteSplitter.js
@@ -5,6 +5,7 @@ const { submitHospitalSurvey } = require("./hospital/api");
 const { initialSubmission } = require("./initialHousehold/api");
 const { log } = require("util-logging");
 
+// Mapping can't be moved to config due to circular dependency (config -> api -> schema -> config)
 const surveyKeyToApiMap = {
   [Surveys.gravedigger.key]: submitGravediggerSurvey,
   [Surveys.hospital.key]: submitHospitalSurvey,
