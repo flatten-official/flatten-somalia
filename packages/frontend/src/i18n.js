53395c2ce8c65a9494c125750a0bde3c2dbb2247
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-08-28 16:53:09 -0400

    Fix capitalization in i18n.js

--- packages/frontend/src/i18n.js
+++ packages/frontend/src/i18n.js
@@ -17,11 +17,10 @@ import footerEN from "./translations/en/Footer.json";
 import homeEN from "./translations/en/Home.json";
 import loadingEN from "./translations/en/Loading.json";
 import navbarEN from "./translations/en/Navbar.json";
-import adminpanelEN from "./translations/en/AdminPanel.json";
 import formIOCustomTextEN from "./translations/en/FormIoCustomText.json";
 import SurveysEN from "./translations/en/Surveys.json";
 import GeneralEN from "./translations/en/General.json";
-import adminEN from "./translations/en/Admin.json";
+import adminPanelEN from "./translations/en/AdminPanel.json";
 
 // FORMIO CONTENT SOMALI
 import formIOAddVolunteerSO from "./translations/so/translation/AddVolunteer.json";
@@ -39,9 +38,9 @@ import footerSO from "./translations/so/Footer.json";
 import homeSO from "./translations/so/Home.json";
 import loadingSO from "./translations/so/Loading.json";
 import navbarSO from "./translations/so/Navbar.json";
-import adminpanelSO from "./translations/so/AdminPanel.json";
 import SurveysSO from "./translations/so/Surveys.json";
 import GeneralSO from "./translations/so/General.json";
+import adminPanelSO from "./translations/so/AdminPanel.json";
 
 const resources = {
   en: {
@@ -62,8 +61,7 @@ const resources = {
     FormIOCustomText: formIOCustomTextEN,
     Surveys: SurveysEN,
     General: GeneralEN,
-    Admin: adminEN,
-    AdminPanel: adminpanelEN,
+    AdminPanel: adminPanelEN,
   },
   so: {
     translation: {
@@ -83,8 +81,7 @@ const resources = {
     FormIOCustomText: formIOCustomTextSO,
     Surveys: SurveysSO,
     General: GeneralSO,
-    Admin: adminEN, // Even in Somali use the English version
-    AdminPanel: adminpanelSO,
+    AdminPanel: adminPanelSO,
   },
 };
 
