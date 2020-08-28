13b7790589daccbfa10ef9075440dedae5fe7346
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-08-28 17:12:40 -0400

    Fix errors

--- packages/frontend/src/backend/volunteer/addVolunteer.js
+++ packages/frontend/src/backend/volunteer/addVolunteer.js
@@ -1,7 +1,7 @@
 import backend from "../api/backend";
 import flattenApi from "../api/api";
 
-const addVolunteer = (volunteerData) => {
+export const addVolunteer = (volunteerData) => {
   return backend.request({
     ...flattenApi.addVolunteer,
     data: { volunteerData },
