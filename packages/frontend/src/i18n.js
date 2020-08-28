b6acbac47fac00ac75636ad3fcebe2178c41f2c7
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-08-28 17:06:33 -0400

    rename adminPanel -> admin in translations

--- packages/frontend/src/i18n.js
+++ packages/frontend/src/i18n.js
@@ -20,7 +20,7 @@ import navbarEN from "./translations/en/Navbar.json";
 import formIOCustomTextEN from "./translations/en/FormIoCustomText.json";
 import SurveysEN from "./translations/en/Surveys.json";
 import GeneralEN from "./translations/en/General.json";
-import adminPanelEN from "./translations/en/AdminPanel.json";
+import adminEN from "./translations/en/Admin.json";
 
 // FORMIO CONTENT SOMALI
 import formIOAddVolunteerSO from "./translations/so/translation/AddVolunteer.json";
@@ -40,7 +40,7 @@ import loadingSO from "./translations/so/Loading.json";
 import navbarSO from "./translations/so/Navbar.json";
 import SurveysSO from "./translations/so/Surveys.json";
 import GeneralSO from "./translations/so/General.json";
-import adminPanelSO from "./translations/so/AdminPanel.json";
+import adminSO from "./translations/so/Admin.json";
 
 const resources = {
   en: {
@@ -61,7 +61,7 @@ const resources = {
     FormIOCustomText: formIOCustomTextEN,
     Surveys: SurveysEN,
     General: GeneralEN,
-    AdminPanel: adminPanelEN,
+    AdminPanel: adminEN,
   },
   so: {
     translation: {
@@ -81,7 +81,7 @@ const resources = {
     FormIOCustomText: formIOCustomTextSO,
     Surveys: SurveysSO,
     General: GeneralSO,
-    AdminPanel: adminPanelSO,
+    AdminPanel: adminSO,
   },
 };
 
