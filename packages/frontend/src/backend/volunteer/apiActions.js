fc7e16b970aff3dd896e3ae399f2f0ca2f18aa9d
Philip-Nicolas <philnic.varga@mail.utoronto.ca>
2020-08-28 17:18:45 -0400

    Move all volunteer api requests into one file

--- packages/frontend/src/backend/volunteer/apiActions.js
+++ packages/frontend/src/backend/volunteer/apiActions.js
@@ -3,3 +3,11 @@ import flattenApi from "../api/api";
 
 export const addVolunteer = (volunteerData) =>
   backend.request({ ...flattenApi.addVolunteer, data: { volunteerData } });
+
+export const listVolunteers = () => backend.request(flattenApi.listVolunteers);
+
+export const changeVolunteerAccess = (access, volunteerId) =>
+  backend.request({
+    ...flattenApi.changeVolunteerAccess,
+    data: { access, volunteerId },
+  });
